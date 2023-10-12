32790b0ba3251f8f4f744f5e9dce5c70477ae9b4
LN27100 <heloceinlovehotmail.fr>
2023-10-12 19:08:05 +0200

    amélioration du design

--- script.js
+++ script.js
@@ -1,54 +1,52 @@
 let nameBody = ["Chaise de développeur web", "L'indispensable du dév", "Ustensile de saison", "Tasse à café en kit", "Pc collector"]
 let imagObject = ["chaise-de-developpeur.JPG", "grain-cafe.jpg", "mouchoir.JPG", "pc-casse.JPG", "tasse-casse.JPG"]
-let testPrice
 let nbrObject = Math.floor(Math.random() * Math.floor(max))
-let compteurTentative
+let compteurTentative = 10
 let imag = document.getElementById('card-img-top')
 let nameObject = document.getElementById('card-title')
 let tentative = document.getElementById('nbrOfTentatives')
 let button = document.getElementById('btn')
 let message = document.getElementById('message')
 let realPrice = automatic()
+let max = 100
 
-
-function automatic() {
-    return Math.floor((Math.random() * 100) + 1)
+function automatic(max) {
+    return Math.floor((Math.random() * max) + 1)
 }
 console.log(realPrice)
 
 
 function imageChoice(tableau) {
-    let numberAleatoire = Math.floor(Math.random() * tableau.length);
-    return tableau[numberAleatoire];
+    return nameBody[Math.floor(Math.random() * tableau.length)]
 }
-let nameBodyAleatoire = choisirObjetAleatoire(nameBody);
-console.log(nameBodyAleatoire);
+let nameBodyAleatoire = imageChoice(nameBody)
+console.log(nameBodyAleatoire)
 
 
 
 function verifNumber() {
-    testPrice = document.getElementById('test-price').value
+    let testPrice = document.getElementById('test-price').value
 
     if (compteurTentative == 0) {
         tentative.innerHTML = "Il vous reste " + compteurTentative + " tentatives."
         message.innerHTML = "Echec, le juste prix était de " + realPrice + " €"
         button.disable = true
     } else {
-        if (tentative > realPrice) {
+        if (testPrice > realPrice) {
             message.innerHTML = "Moins"
             compteurTentative--
             tentative.innerHTML = "Il vous reste " + compteurTentative + " tentatives."
         }
-        if (tentative < realPrice) {
+        if (testPrice < realPrice) {
             message.innerHTML = "Plus"
             compteurTentative--
             tentative.innerHTML = "Il vous reste " + compteurTentative + " tentatives."
         }
-        if (tentative == realPrice) {
+        if (testPrice == realPrice) {
             message.innerHTML = "Vicoire, vous remportez l'objet !"
             tentative.innerHTML = "Il vous reste " + compteurTentative + " tentatives."
             button.disable = true
         }
     }
 }
-button.addEventListener('click', verifNumber, false)
+button.addEventListener('click', verifNumber)
